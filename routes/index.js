009718ca582f9f7d0ce6c93b769d0b22f248e1ef
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2023-02-11 14:29:51 -0800

    intialization

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// Connect controller methods to their corresponding routes
+router.get('/products', controller.products.get);
+
+router.get('/products/:id', controller.products.getProduct);
+
+router.get('/products/:product_id/styles',
+    controller.products.getProductStylesAndPhotos);
+
+
+router.get('/products/:product_id/related',
+    controller.products.getProductRelated);
+
+module.exports = router;
